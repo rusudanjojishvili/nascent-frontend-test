4e29420cdd2c9cd9f309b2c012d7985226de4506
ruskajojishvili <rosjojishvili@gmail.com>
2025-11-16 11:56:59 +0400

    remove test manual server url

--- src/api/orderBook.ts
+++ src/api/orderBook.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Asset, RawOrderBook, ParsedOrderBook, OrderRequest, Trade } from '../types';
 import { parseOrderBook } from '../utils/table';
 
-const API_URL = "https://nascent-frontend-test-deploy-server.onrender.com";
+const API_URL = process.env.REACT_APP_API_URL || "";
 // Fetch orderbook and parse prices/quantities to numbers
 export const fetchOrderBook = async (asset: Asset): Promise<ParsedOrderBook> => {
     const response = await axios.get<RawOrderBook>(`${API_URL}/orderbook/${asset}`);
